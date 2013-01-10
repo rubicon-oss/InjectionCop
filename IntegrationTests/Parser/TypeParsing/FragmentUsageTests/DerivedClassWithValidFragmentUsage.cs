714f157098350a8863ccd9c691c7031f8242eefc
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2013-01-10 15:55:38 +0100

    added validation rule for fragment usages.

--- IntegrationTests/Parser/TypeParsing/FragmentUsageTests/DerivedClassWithValidFragmentUsage.cs
+++ IntegrationTests/Parser/TypeParsing/FragmentUsageTests/DerivedClassWithValidFragmentUsage.cs
@@ -4,7 +4,7 @@ namespace InjectionCop.IntegrationTests.Parser.TypeParsing.FragmentUsageTests
 {
   public class DerivedClassWithValidFragmentUsage : BaseClass
   {
-    public void Foo ([Fragment("ValidFragmentType")] int parameter1, string parameter2)
+    public override void Foo ([Fragment("ValidFragmentType")] int parameter1, string parameter2)
     {
     }
   }
