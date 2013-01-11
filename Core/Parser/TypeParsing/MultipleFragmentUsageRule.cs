8bf17965da75c11771471021091e7dc0e0e4ad32
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2013-01-11 11:30:44 +0100

    added validation rule for multiple fragment usage.

---
+++ Core/Parser/TypeParsing/MultipleFragmentUsageRule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using InjectionCop.Parser.ProblemPipe;
+using InjectionCop.Utilities;
+using Microsoft.FxCop.Sdk;
+using System.Linq;
+
+namespace InjectionCop.Parser.TypeParsing
+{
+  public class MultipleFragmentUsageRule : BaseFxCopRule
+  {
+    public MultipleFragmentUsageRule ()
+        : base ("MulitpleFragmentUsageRule")
+    {
+    }
+
+    public override ProblemCollection Check (Member member)
+    {
+      if (member is Method)
+      {
+        var method = (Method) member;
+        Parse (method);
+      }
+      return Problems;
+    }
+
+    private void Parse (Method method)
+    {
+      foreach (var parameter in method.Parameters)
+      {
+        var fragmentCount = parameter.Attributes.Count(FragmentUtility.IsFragment);
+
+        if (fragmentCount > 1)
+          AddProblem (parameter.SourceContext, parameter.Name);
+      }
+    }
+
+    private void AddProblem (SourceContext sourceContext, Identifier parameterName)
+    {
+      var resolution = GetResolution (parameterName.Name);
+      var problem = new Problem (resolution, sourceContext, CheckId);
+      Problems.Add (problem);
+    }
+  }
+}
---
+++ IntegrationTests/Parser/TypeParsing/FragmentUsageTests/MulitpleFragmentUsageRuleTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using InjectionCop.Parser.TypeParsing;
+using InjectionCop.Utilities;
+using Microsoft.FxCop.Sdk;
+using NUnit.Framework;
+
+namespace InjectionCop.IntegrationTests.Parser.TypeParsing.FragmentUsageTests
+{
+  [TestFixture]
+  public class MulitpleFragmentUsageRuleTest
+  {
+    protected readonly string c_InjectionCopRuleId = "IC0004";
+
+    [Test]
+    public void CheckMember_InvalidFragmentUsage ()
+    {
+      var sampleMethod = GetSampleMethod ("InvalidFragmentUsage");
+
+      var rule = new MultipleFragmentUsageRule();
+      var result = rule.Check (sampleMethod);
+      Assert.That (TestHelper.ContainsProblemID (c_InjectionCopRuleId, result), Is.True);
+    }
+
+    [Test]
+    public void CheckMember_FormatsMessage ()
+    {
+      var sampleMethod = GetSampleMethod ("InvalidFragmentUsage");
+
+      var rule = new MultipleFragmentUsageRule();
+      var result = rule.Check (sampleMethod);
+      Assert.That (result[0].Resolution.ToString(), Is.EqualTo("Parameter 'parameter' has multiple fragment types assigned."));
+    }
+
+    [Test]
+    public void CheckMember_ValidFragmentUsage ()
+    {
+      var sampleMethod = GetSampleMethod ("ValidFragmentUsage");
+
+      var rule = new MultipleFragmentUsageRule();
+      var result = rule.Check (sampleMethod);
+      Assert.That (result, Is.Empty);
+    }
+
+    [Test]
+    public void CheckMember_NoFragmentUsage()
+    {
+      var sampleMethod = GetSampleMethod ("NoFragmentUsage");
+
+      var rule = new MultipleFragmentUsageRule();
+      var result = rule.Check (sampleMethod);
+      Assert.That (result, Is.Empty);
+    }
+
+    private Method GetSampleMethod (string methodName)
+    {
+      return IntrospectionUtility.MethodFactory<ClassWithMultipleFragments> (
+          methodName, IntrospectionUtility.TypeNodeFactory<int>());
+    }
+  }
+}
